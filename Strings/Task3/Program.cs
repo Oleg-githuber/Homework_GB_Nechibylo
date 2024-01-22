9e437787594f52f9a76498bf58d0ab8023413551
Oleg <oilminer@mail.ru>
2024-01-22 17:47:14 +0500

    Третье задание

---
+++ Strings/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите строку: ");
+string str1 = Console.ReadLine();
+bool isPalindrome = true;
+for (int i = 0; i < (str1.Length / 2); i++) {
+    if (str1[i] != str1[str1.Length -1 - i]) {
+        isPalindrome = false;
+    } 
+}
+if (isPalindrome) Console.WriteLine("Строка является палиндромом");
+else Console.WriteLine("Строка не является палиндромом");
