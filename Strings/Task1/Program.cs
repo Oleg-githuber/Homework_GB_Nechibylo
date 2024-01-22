d66ffabfc0253f6335472b6d0a6f501012c72ac8
Oleg <oilminer@mail.ru>
2024-01-22 17:29:11 +0500

    Задание 1

---
+++ Strings/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿char [,] ch1 = {{'a', 'b', 'c'},
+                {'d', 'e', 'f'},
+                {'g', 'h', 'i'}};
+int stringSize = ch1.Length;
+string str1 = "";
+foreach(char e in ch1) {
+    str1 += e;
+}
+Console.WriteLine(str1);
