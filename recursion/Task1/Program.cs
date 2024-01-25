f372281849a776eb2e51bb7961f386e9704cac94
Oleg <oilminer@mail.ru>
2024-01-25 16:18:13 +0500

    Добавил рекурсивную функцию

--- recursion/Task1/Program.cs
+++ recursion/Task1/Program.cs
@@ -1,14 +1,14 @@
 ﻿/* Программа вывода в консоль всех  натуральных чисел от M до N */
 
 // Функция ввода первого числа
-int Number1() (
+int SetNumber1() (
     Console.WriteLine("Введите первое натуральное число: ");
     int m = Int32.Parse(Console.ReadLine());
     return m;
 )
 
 // Функция ввода первого числа
-int Number2() (
+int SetNumber2() (
     Console.WriteLine("Введите второе натуральное число: ");
     int n = Int32.Parse(Console.ReadLine());
     return n;
@@ -27,3 +27,15 @@ int Min(int m, int n) {
     if (m > n) return n;
     else return m;
 }
+
+// Рекурсивная функция
+void PrintNumbers(int m, int n) {
+    if (m > n) return;
+    else 
+    {
+        Console.Write($"{m}\t");
+        PrintNumbers(m + 1, n);
+    
+    }
+}
+
