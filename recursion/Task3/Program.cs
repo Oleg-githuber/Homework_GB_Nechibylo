5cc472ef376a7b3102665cda431b9e5a4d4e19a8
Oleg <oilminer@mail.ru>
2024-01-25 17:22:59 +0500

    Задача 3

---
+++ recursion/Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Программа для обратного вывода массива, с помощью рекурсии
+
+
+// Функция получения массива
+int [] GetArray() {
+    int [] arr = {1, 2, 5, 0, 10, 34};
+    return arr;
+}
+
+void ShowReverseArray(int [] array, int index) {
+    if (index == array.Length) return;
+    else {
+        ShowReverseArray(array, index + 1);
+        Console.Write($"{array[index]}\t");
+    }
+}
+
+void ShowArray(int [] array, int index) {
+    if (index == array.Length) return;
+    else {
+        
+        Console.Write($"{array[index]}\t");
+        ShowArray(array, index + 1);
+    }
+}
+
+Console.Clear();
+int [] arr = GetArray();
+Console.WriteLine("Ваш массив:\n");
+ShowArray(arr, 0);
+Console.WriteLine("\nОбработаный массив:\n");
+ShowReverseArray(arr, 0);
